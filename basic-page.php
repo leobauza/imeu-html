fc7c2a7b7e61cb93e6be7ef6b07b0be20ff1ff87
Leo <leonardobauza@gmail.com>
2014-04-09 16:11:37 -0400

    sidebar form

--- basic-page.php
+++ basic-page.php
@@ -10,6 +10,25 @@
 
 			<?php include($_SERVER['DOCUMENT_ROOT'] . '/inc/popular-tags.php'); ?>
 
+			<div class="sidebar__form">
+				<form action="http://imeu.createsend.com/t/r/s/zttkr/" method="post">
+				  <h4>Sign Up</h4>
+					<div class="form__group">
+						<label for="fieldName">Name</label>
+						<input id="fieldName" name="cm-name" type="text" required />
+					</div>
+					<div class="form__group">
+						<label for="fieldEmail">Email</label>
+						<input id="fieldEmail" name="cm-zttkr-zttkr" type="email" required />
+					</div>
+					<div class="form__group">
+						<label for="fieldkkpt">Organization</label>
+						<input id="fieldkkpt" name="cm-f-kkpt" type="text" required />
+					</div>
+					<button class="btn--default" type="submit">Subscribe</button>
+				</form>
+			</div>
+
 			<a href="#" class="btn--donate">Donate to the IMEU</a>
 
 		</aside>
